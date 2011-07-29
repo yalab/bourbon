90b3b21fe0cf0e613f2febb4a07ee443a61b3912
yalab <rudeboyjet@gmail.com>
2011-07-29 17:12:34 +0900

    Re render after download.

--- src/main/scala/MainActivity.scala
+++ src/main/scala/MainActivity.scala
@@ -3,7 +3,7 @@ package org.yalab.bourbon
 import java.lang.Runnable
 import _root_.android.app.{Activity, ListActivity, ProgressDialog}
 import _root_.android.content.{ContentValues}
-import _root_.android.os.Bundle
+import _root_.android.os.{Bundle, Handler}
 import _root_.android.widget.{TextView, ListView, SimpleCursorAdapter}
 import _root_.android.view.{Menu, MenuItem}
 
@@ -13,11 +13,17 @@ object MainActivity {
 
 class MainActivity extends ListActivity {
   import MainActivity._
+  val handler = new Handler
+
   override def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
+    render
+  }
+
+  def render{
     val c = getContentResolver.query(ArticleProvider.CONTENT_URI, Array("title"), null, null, null)
 
-    val adapter = new SimpleCursorAdapter(this, R.layout.row, c,
+    val adapter = new SimpleCursorAdapter(MainActivity.this, R.layout.row, c,
                                           Array("title"), Array(_root_.android.R.id.title))
     setListAdapter(adapter)
   }
@@ -33,6 +39,7 @@ class MainActivity extends ListActivity {
       case OPTION_DOWNLOAD => {
         val dialog = ProgressDialog.show(MainActivity.this, "",
                                          "Downloading. Please wait...", true, true)
+
         (new Thread(new Runnable(){
           def run() {
             val resolver = getContentResolver
@@ -42,6 +49,7 @@ class MainActivity extends ListActivity {
               resolver.insert(ArticleProvider.CONTENT_URI, values)
             })
             dialog.dismiss
+            handler.post(new Runnable() { def run { render } });
           }
         })).start
         true
