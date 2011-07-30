26732d6d9eb7fc1c4ed3c6c443210df2b78bd3cb
yalab <rudeboyjet@gmail.com>
2011-07-30 11:12:14 +0900

    Use webview.

--- src/main/scala/activities/ArticleActivity.scala
+++ src/main/scala/activities/ArticleActivity.scala
@@ -3,17 +3,17 @@ package org.yalab.bourbon
 import _root_.android.app.Activity
 import _root_.android.os.Bundle
 import _root_.android.widget.TextView
+import _root_.android.webkit.WebView
 
 class ArticleActivity extends Activity {
   override def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.article)
-    val fields = Array("title", "script", "mp3")
+    val fields = Array("script")
     val c = getContentResolver.query(getIntent.getData, fields, null, null, null)
     c.moveToFirst
-    fields.foreach(f => {
-      val id = getResources.getIdentifier(f, "id", "org.yalab.bourbon")
-      findViewById(id).asInstanceOf[TextView].setText(c.getString(c.getColumnIndex(f)))
-    })
+    val script = c.getString(c.getColumnIndex("script"))
+    val webview = findViewById(R.id.webview).asInstanceOf[WebView]
+    webview.loadData(script, "text/html", "utf-8")
   }
 }
