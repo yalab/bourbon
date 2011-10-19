bc7aa2db11e4de9be2eb394d1e83b83424e3de51
yalab <rudeboyjet@gmail.com>
2011-10-19 19:44:43 +0900

    versionCode upto 4.

--- project/build.scala
+++ project/build.scala
@@ -6,7 +6,7 @@ import AndroidKeys._
 object General {
   val settings = Defaults.defaultSettings ++ Seq (
     name := "Bourbon",
-    version := "0.1",
+    version := "4",
     scalaVersion := "2.9.1",
     platformName in Android := "android-8"
   )
