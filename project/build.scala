863f5920924c1c6251f17444778bcc5870debaad
yalab <rudeboyjet@gmail.com>
2011-10-19 19:43:57 +0900

    Scala version up to 2.9.1.

--- project/build.scala
+++ project/build.scala
@@ -7,7 +7,7 @@ object General {
   val settings = Defaults.defaultSettings ++ Seq (
     name := "Bourbon",
     version := "0.1",
-    scalaVersion := "2.9.0-1",
+    scalaVersion := "2.9.1",
     platformName in Android := "android-8"
   )
 
